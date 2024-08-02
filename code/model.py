00b08b53b338530c8b1dacf5a32a42b172029f3d
Gargi <gargirajadnya@gmail.com>
2024-08-02 18:27:52 +0100

    model code

---
+++ code/model.py
@@ -0,0 +1,17 @@
+#%%
+#loading libraries
+#basic
+import pandas as pd
+import numpy as np
+
+
+#standardization
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+
+#splitting data
+from sklearn.model_selection import train_test_split
+
+#%%

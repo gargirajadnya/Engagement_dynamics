cfbf03d594e4283cfaf45aefbad589cf789d5b1c
Gargi <gargirajadnya@gmail.com>
2024-08-03 22:44:12 +0100

    svr

--- code/2_data_exp_manip.py
+++ code/2_data_exp_manip.py
@@ -21,6 +21,7 @@ from sklearn.model_selection import train_test_split
 
 #xgboost
 import xgboost as xgb
+from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 
@@ -201,3 +202,19 @@ print("Mean Absolute Error:", mae)
 print("R-squared:", r2)
 
 # %%
+svr_model = SVR(kernel='rbf', C=100, gamma='scale', epsilon=0.1)
+svr_model.fit(X_train_pca, y_train)
+
+# Make predictions on the test set
+y_pred = svr_model.predict(X_test_pca)
+
+# Evaluate the model
+rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+mae = mean_absolute_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print('SVR RMSE:', rmse)
+print('SVR MAE:', mae)
+print('SVR R-squared:', r2)
+
+#%%
